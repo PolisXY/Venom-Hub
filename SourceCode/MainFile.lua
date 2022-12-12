061653824f13386dd3d8e9fb9ea70b146b18e272
Xing Poe <83317840+PolisXY@users.noreply.github.com>
2022-12-11 21:36:35 -0800

    Create MainFile.lua

---
+++ SourceCode/MainFile.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 155615604 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/PolisXY/Venom-Hub/main/SourceCode/PrisonLife.lua"))()
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/PolisXY/Venom-Hub/main/SourceCode/Universal.lua"))()
+end
